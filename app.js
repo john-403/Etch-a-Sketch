3ec7d073dd1b042b3e8e0d6a81295176bcd68084
john-403 <john.habib012@gmail.com>
2022-06-11 11:09:12 +0300

    finished logic for Etch-a-Sketch and added customization for the number of squares per side

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
 const container = document.querySelector('#container');
+button = document.querySelector('button');
+
+container.addEventListener('mouseover', function (event) {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    event.target.style.backgroundColor = '#' + randomColor;
+    // container.style.border = 'none'
+    setTimeout(function () {
+        event.target.style.backgroundColor = "";
+    }, 500)
+})
+
+
+
+button.addEventListener('click', function (event) {
+    var nbofsquaresperside = parseInt(prompt('Enter number of squares per side'));
+    container.innerHTML = '';
+    for (let i = 0; i < nbofsquaresperside ** 2; i++) {
+        const div = document.createElement('div');
+        div.style.border = '1px solid grey';
+        // div.style.width = '15px';
+        // div.style.height = '15px';
+
+        container.appendChild(div);
+
+        div.style.width = (272 / nbofsquaresperside) - 2 + 'px';
+        div.style.height = (272 / nbofsquaresperside) - 2 + 'px';
+
+    }
+
+
+})
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
@@ -9,11 +40,4 @@ for (let i = 0; i < 256; i++) {
 
     container.appendChild(div);
 }
-
-container.addEventListener('mouseover', function (event) {
-    event.target.style.backgroundColor = 'black';
-    // container.style.border = 'none'
-    setTimeout(function () {
-        event.target.style.backgroundColor = "";
-    }, 500)
-})
+container.style.width = 16 * 17 + 'px';
